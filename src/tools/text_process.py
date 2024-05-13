834aa26703dcba2b07ee7124e72fbf184c6058c3
shun31y <shun.kato@elyza.ai>
2024-05-13 10:23:10 +0900

    全角を半角に統一する系の関数の追加

--- src/tools/text_process.py
+++ src/tools/text_process.py
@@ -1,14 +1,11 @@
 from ..utils.regex import (
-    captured_group,
-    uncaptured_group,
     concat_regex,
     follow_regex,
-    not_follow_regex,
     precede_regex,
-    not_precede_regex,
 )
 import re
 import demoji
+import unicodedata
 
 
 def remove_inline(text: str) -> str:
@@ -45,3 +42,31 @@ def insert_newline_before_regex(text: str, regexes: list[str]) -> str:
     regexes = concat_regex(regexes)
     cleansed = re.sub(precede_regex(regex="", regex_obj=regexes), "\n", text)
     return cleansed
+
+
+def full_to_half_width(text: str) -> str:
+    return unicodedata.normarize("NFKC", text)
+
+
+def remove_full_width_symbol(text: str) -> str:
+    """全角記号を削除する関数。削除できる全角文字は以下の通り
+    全角の ！"＃＄％＆'（）＊＋，－．／
+    全角の ：；＜＝＞？＠
+    全角の ［＼］＾＿｀
+    全角の ｛｜｝～
+    CJKの記号と句読点 、。〃〄々〆〇〈〉《》「」『』【】〒〓〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿
+    全角スペース
+
+    Args:
+        text (str): 処理前のテキスト
+
+    Returns:
+        str: 削除されたテキスト。
+    """
+    return re.sub(
+        "[\uFF01-\uFF0F\uFF1A-\uFF20\uFF3B-\uFF40\uFF5B-\uFF65\u3000-\u303F]", "", text
+    )
+
+
+def convert_fullwidth_space_to_halfwidth(text: str) -> str:
+    return re.sub(r"\u3000", " ", text)
